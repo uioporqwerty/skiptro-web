378ea40fea598275fe689c1cefd8ad3ff4ac0f96
uioporqwerty <nitish.sachar@protonmail.com>
2023-03-11 10:35:48 -0800

    Fix webpack

--- webext.config.js
+++ webext.config.js
@@ -3,6 +3,21 @@ const webpack = require('webpack');
 module.exports = {
     webpack: (config) => {
         config.output.hashFunction = 'sha256';
+        config.output.filename = 'main.js';
+        config.entry = './content-script.ts';
+        config.resolve = {
+            extensions: ['.js', '.json', '.mjs', '.jsx', '.ts']
+        };
+        config.module = {
+            rules: [
+                {
+                    test: /\.tsx?/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        };
+
         console.dir(config);
         return config;
     }
