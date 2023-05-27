13d113b0b9cf3b153c2696bc4b3545590ca6b1c0
uioporqwerty <nitish.sachar@protonmail.com>
2023-05-27 09:13:44 -0700

    fic formatting

--- app/services/logging/console-logging-service.ts
+++ app/services/logging/console-logging-service.ts
@@ -3,8 +3,8 @@ import { LoggingService } from './logging-service';
 export class ConsoleLoggingService implements LoggingService {
     private tag = '[Skiptro]';
 
-    constructor(private readonly currentDate: Date = new Date()) { }
-    
+    constructor(private readonly currentDate: Date = new Date()) {}
+
     debug(message: string): void {
         console.debug(`${this.tag} ${message} ${this.addTimestamp()}`);
     }
