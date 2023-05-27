aa804875daab4aa85e32e80828c58696260f9000
uioporqwerty <nitish.sachar@protonmail.com>
2023-05-27 08:55:32 -0700

    Fix linting issues

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Component } from 'react';
 import { InjectorContext } from '../../InjectorContext';
 import { Ii18nService } from '../../services/i18n/i18n-service';
--- app/components/popup/index.tsx
+++ app/components/popup/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Component } from 'react';
 import { InjectorContext } from '../../InjectorContext';
 import { Footer } from '../footer';
