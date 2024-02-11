2df96785fc0f09748409da8a90e6c978083ca61a
uioporqwerty <nitish.sachar@protonmail.com>
2024-02-10 23:06:38 -0800

    Add styling to popup window

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Component } from 'react';
+import { Component, HTMLAttributes } from 'react';
 import { InjectorContext } from '../../InjectorContext';
 import { Ii18nService } from '../../services/i18n/i18n-service';
 import './index.css';
 
-interface Props {
+type HeaderProps = {
     title?: string;
-}
+    size?: 'h1' | 'h2' | 'h3' | 'h4';
+} & HTMLAttributes<HTMLDivElement>;
 
-class HeaderComponent extends Component<Props> {
+class HeaderComponent extends Component<HeaderProps> {
     private i18n: Ii18nService;
 
-    constructor(props: Props, context: any) {
+    constructor(props: HeaderProps, context: any) {
         super(props, context);
         this.i18n = context.resolve('i18n');
     }
 
     render() {
+        const Heading = this.props.size ?? 'h1';
+
         return (
-            <div className="header-container">
-                <h1 className="header-title">
+            <div className={`${this.props.className} header-container`}>
+                <Heading className="header-title">
                     {this.props.title ||
                         this.i18n.getTranslation('header_title')}
-                </h1>
+                </Heading>
             </div>
         );
     }
--- app/components/popup/index.tsx
+++ app/components/popup/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
 import { Component } from 'react';
+import { Ii18nService } from '../../services/i18n/i18n-service';
 import { InjectorContext } from '../../InjectorContext';
 import { Footer } from '../footer';
 import { Header } from '../header';
 import './index.css';
 
+type PopUpProps = {};
 class PopUpComponent extends Component {
+    private i18n: Ii18nService;
+
+    constructor(props: PopUpProps, context: any) {
+        super(props, context);
+        this.i18n = context.resolve('i18n');
+    }
+
     render() {
         return (
             <div className="popup-container">
-                <Header />
+                <Header className="popup-header" />
+                <Header
+                    className="popup-tagline"
+                    size="h2"
+                    title={this.i18n.getTranslation('popup_tagline')}
+                />
                 <div className="popup-content"></div>
                 <Footer />
             </div>
